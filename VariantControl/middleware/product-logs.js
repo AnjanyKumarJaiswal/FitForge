31b3b74f4fbf56909de3b24bc87b7b6f101e2cf3
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-03-25 10:32:49 +0530

    added logs for keeping the record

--- VariantControl/middleware/product-logs.js
+++ VariantControl/middleware/product-logs.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 
-
 function loggedinfo(filename){
-    return (req,res,next)=>{
-        fs.appendFile('log.txt',`Date:${Date.now()} , IP:${req.ip} ${req.method}: ${req.path} \n`,
+    return  (req,res,next)=>{
+         fs.appendFile(filename,`Date:${Date.now()} , IP:${req.ip} ${req.method}: ${req.path} \n`,
         (err,data)=>{
             next();
         });
